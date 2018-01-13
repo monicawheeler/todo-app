76932d5610b01005c9c1792c1d40db48cb13413b
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-12 20:06:45 -0600

    Create database.sql file

---
+++ database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE categories (
+	id SERIAL PRIMARY KEY,
+	description VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tasks (
+	id SERIAL PRIMARY KEY,
+	description VARCHAR(255) NOT NULL,
+	complete BOOLEAN NOT NULL DEFAULT 'No',
+	date_created DATE,
+	category_id INT REFERENCES "categories"
+);
+
+-- Sample dataset
+INSERT INTO categories (description) 
+VALUES ('Personal'), ('Work'), ('Chores');
+
+INSERT INTO tasks (description, category_id) 
+VALUES ('Get hair cut', 1), ('Shop for new shoes', 1), ('Complete todo application', 2), ('Start koala application', 2), ('Wash the dishes', 3), ('Do laundry', 3);
