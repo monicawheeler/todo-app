d1ae79d0c3501b5929bc3d02887ac0e0b2a9bde1
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-13 07:43:04 -0600

    Add export in tasks.route

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-console.log('hit the tasks router file');
-
 // get all tasks
 router.get('/', (req, res) => {
-    console.log('got inside the router');
-    
     const queryText = 'SELECT * FROM tasks';
     pool.query(queryText)
         // runs on success
@@ -19,3 +15,5 @@ router.get('/', (req, res) => {
             res.sendStatus(500);
         });
 });
+
+module.exports = router;
