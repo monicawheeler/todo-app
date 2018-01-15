0ef099950f99f3cfb69e924672369a6d51d2ce26
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-15 16:22:57 -0600

    Update for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,36 @@
 const pg = require('pg');
 const Pool = pg.Pool;
-const config = {
-    database: 'todo_app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 5000
-};
+const config = {};
+
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
+  
+    config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true, // heroku requires ssl to be true
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  
+  } else {
+    // only change the things on the right side of the ||
+    config = {
+      user: process.env.PG_USER || null, //env var: PGUSER
+      password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+      host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+      port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+      database: process.env.DATABASE_NAME || 'todo_app', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  }
 
 // create instance of pool object
 const pool = new Pool(config);
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const tasksRouter = require('./routes/tasks.router');
 const app = express();
-const PORT = 2828;
+const PORT = process.env.PORT || 2828;
 
 // body parser
 app.use(bodyParser.urlencoded({extended:true}));
